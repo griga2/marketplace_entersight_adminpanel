786e11d3f139341f2b80ce2c5de25e1e81e1eb63
griga2 <griga2999wow@gmail.com>
2024-10-29 22:20:34 +0700

    awd

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,12 @@
 import {createRouter, createWebHistory} from "vue-router";
 import LayoutView from '../views/LayoutVuew/LayoutVuew.vue'
   const routes = [
-    {name: 'layout', path: '/', component: LayoutView},
+    {name: 'layout', path: '/', component: LayoutView,
+      child: [
+        {name: 'products', path: '/products', component: LayoutView},
+      ]
+    },
+    {name: 'auth', path: '/auth', component: LayoutView}
   ]
 
   const router = createRouter({
